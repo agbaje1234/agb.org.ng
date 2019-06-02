b15c3d93f6860087122e33ba90226ae63012ea6c
agbaje1234 <51216723+agbaje1234@users.noreply.github.com>
2019-06-01 17:54:24 -0700

    folder

---
+++ folder/js/js.js
@@ -0,0 +1,18 @@
+function Contact(){
+		alert("You can contect me on Facebook:oluwashola.agbajestephen.7");
+		alert("You can also contect me on Email: agbajestephen064@yahoo.com or call: 08181782919");
+}
+
+function About(){
+		alert("About this site");
+		//prompt("This site is all about u learning how to create a simple web page");
+}
+
+
+function Home(){
+		alert("You are still on the Home page");
+}
+
+function Back(){
+		alert("Back to the previous page");
+}
